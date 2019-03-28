d3179b051355309fbdd7e87718910d21a1ae2316
username <email>
2019-03-28 15:53:25 +0800

    first commit

--- src/channel/channelMain.go
+++ src/channel/channelMain.go
@@ -8,8 +8,8 @@ import (
 
 
 func add(a int,b int,pipe chan int){
-	var sum int
-	sum = a + b
+	//var sum int
+	sum := a + b
 	
 	pipe <- sum
 
@@ -17,12 +17,12 @@ func add(a int,b int,pipe chan int){
 
 func main(){ 
 
-	var pipe chan int //"chan int" is to defined the pipeline for integer type
-	pipe = make(chan int,10)
+	//var pipe chan int //"chan int" is to defined the pipeline for integer type
+	pipe := make(chan int,10)
 	for i:=0;i<10;i++{
 	go add(i,i+1,pipe)
-	var result int
-	result = <-pipe
+	//var result int
+	result := <-pipe
 	fmt.Println("result:" + strconv.Itoa(result))
 	}
 	//time.Sleep(5 * time.Second)
